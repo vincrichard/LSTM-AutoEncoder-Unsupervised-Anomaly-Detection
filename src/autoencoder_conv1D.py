a534e44ec6aacc18429c58b2634b75243869f1b2
vincrichard <vincent.richard1094@gmail.com>
2020-04-17 15:15:25 +0200

    test bigger stride

--- src/autoencoder_conv1D.py
+++ src/autoencoder_conv1D.py
@@ -2,16 +2,16 @@ import torch.nn as nn
 
 
 class AutoEncoderConv1D(nn.Module):
-    def __init__(self, x_dim, conv_dim1, kernel_lenght, h_dim1, h_dim2, z_dim):
+    def __init__(self, x_dim, conv_dim1, kernel_length, stride, h_dim1, h_dim2, z_dim):
         super(AutoEncoderConv1D, self).__init__()
 
         # encoder part
         self.encoder = nn.Sequential(
-            nn.Conv1d(in_channels=1, out_channels=conv_dim1, kernel_size=(1, kernel_lenght), stride=1, padding=0, bias=True),
+            nn.Conv1d(in_channels=1, out_channels=conv_dim1, kernel_size=(1, kernel_length), stride=stride, padding=0, bias=True),
             nn.BatchNorm2d(conv_dim1),
             nn.ReLU(True),
             nn.Flatten(),
-            nn.Linear(in_features=(x_dim-kernel_lenght +1)*conv_dim1, out_features=h_dim1, bias=True),
+            nn.Linear(in_features=((x_dim-kernel_length)/stride +1)*conv_dim1, out_features=h_dim1, bias=True),
             nn.BatchNorm1d(h_dim1),
             nn.ReLU(True),
             nn.Linear(in_features=h_dim1, out_features=h_dim2, bias=True),
