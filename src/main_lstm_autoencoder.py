e25417089af71ba1f8a9e44bd575d3124ce07811
vincrichard <vincent.richard1094@gmail.com>
2020-04-23 23:37:47 +0200

    update LSTM AE

--- src/main_lstm_autoencoder.py
+++ src/main_lstm_autoencoder.py
@@ -1,6 +1,8 @@
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
 from src.utils.airbus_data import AirbusData
 from src.model.LSTM_auto_encoder import LSTMAutoEncoder
 from src.utils.callbacks import EarlyStopping
@@ -17,17 +19,21 @@ device = torch.device('cpu')
 # ----------------------------------#
 
 train_seq = AirbusData('../data/train_seq_mean_max_min.pt', type='pytorch')
-train_loader = DataLoader(train_seq, batch_size, shuffle=True, drop_last=True)
+train_loader = DataLoader(train_seq, batch_size, shuffle=True)
 seq_length, size_data, nb_feature = train_seq.data.shape
 
 valid_seq = AirbusData('../data/valid_seq_mean_max_min.pt', type='pytorch')
-valid_loader = DataLoader(valid_seq, batch_size, shuffle=True, drop_last=True)
+valid_loader = DataLoader(valid_seq, batch_size, shuffle=True)
+
+test_seq = AirbusData('../data/test_seq_mean_max_min.pt', type='pytorch')
+test_loader = DataLoader(test_seq, batch_size, shuffle=False)
 
 # ----------------------------------#
 #         build model              #
 # ----------------------------------#
 
-model = LSTMAutoEncoder(num_layers=1, batch_size=batch_size, hidden_size=100, nb_feature=nb_feature, device=device)
+model = LSTMAutoEncoder(num_layers=1, hidden_size=100, nb_feature=nb_feature, device=device)
+model = model.to(device)
 # optimizer
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 # loss
@@ -36,8 +42,8 @@ criterion = torch.nn.MSELoss()
 earlyStopping = EarlyStopping(patience=5)
 model_management = ModelManagement(path_model, name_model)
 # Plot
-plot_loss_train = LossCheckpoint()
-plot_loss_valid = LossCheckpoint()
+loss_checkpoint_train = LossCheckpoint()
+loss_checkpoint_valid = LossCheckpoint()
 
 
 def train(epoch):
@@ -48,7 +54,7 @@ def train(epoch):
         # forward
         data = data.to(device)
         output = model.forward(data)
-        loss = criterion(data, output)
+        loss = criterion(data, output.to(device))
         # backward
         loss.backward()
         train_loss += loss.item()
@@ -62,16 +68,17 @@ def train(epoch):
 
     avg_loss = train_loss / len(train_loader)
     print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, avg_loss))
-    plot_loss_train.losses.append(avg_loss)
+    loss_checkpoint_train.losses.append(avg_loss)
 
 
 def evaluate(loader, validation=False, epoch=0):
+    model.eval()
     eval_loss = 0
     with torch.no_grad():
         for id_batch, data in enumerate(loader):
             data = data.to(device)
             output = model.forward(data)
-            loss = criterion(data, output)
+            loss = criterion(data, output.to(device))
             eval_loss += loss.item()
         print('\r', 'Eval [{}/{} ({:.0f}%)] \tLoss: {:.6f})]'.format(
             id_batch + 1, len(loader),
@@ -81,9 +88,29 @@ def evaluate(loader, validation=False, epoch=0):
     print('====> Validation Average loss: {:.4f}'.format(avg_loss))
     # Checkpoint
     if validation:
-        plot_loss_valid.losses.append(avg_loss)
+        loss_checkpoint_valid.losses.append(avg_loss)
         model_management.checkpoint(epoch, model, optimizer, avg_loss)
         return earlyStopping.check_training(avg_loss)
+
+
+def compare_ts(model, loader):
+    """
+    Plot a random original multivariate sequence from loader and the predict one
+    """
+    ind = np.random.randint(0, loader.batch_size)
+    model.eval()
+    with torch.no_grad():
+        for id_batch, data in enumerate(loader):
+            data = data.to(device)
+            output = model.forward(data)
+            loss = criterion(data[:, ind, :], output[:, ind, :].to(device))
+            plt.plot(data[:, ind, :].cpu().numpy())
+            plt.plot(output[:, ind, :].numpy())
+            plt.title('loss= %f' % loss.item())
+            plt.legend(['data mean', 'data max', 'data min',
+                        'predict mean', 'predict max', "predict min"],
+                       bbox_to_anchor=(1.1, 1.05))
+            break
 
 
 if __name__ == "__main__":
@@ -91,4 +118,3 @@ if __name__ == "__main__":
         train(epoch)
         if evaluate(valid_loader, validation=True, epoch=epoch):
             break
-
